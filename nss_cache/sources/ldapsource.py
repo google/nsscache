a2b301b6062d8cbce035491542b5b6b88cf71e7b
Brandon Azer <azerbran@msu.edu>
2017-11-02 13:42:48 -0400

    fix style

--- nss_cache/sources/ldapsource.py
+++ nss_cache/sources/ldapsource.py
@@ -302,7 +302,7 @@ class LdapSource(source.Source):
               # else: An empty cookie means we are done.
 
           # break loop once result3 doesn't time out and reset cookie
-          setCookieOnControl(self.ldap_controls,'',self.PAGE_SIZE)
+          setCookieOnControl(self.ldap_controls, '', self.PAGE_SIZE)
           break
         except ldap.SIZELIMIT_EXCEEDED:
           self.log.warning('LDAP server size limit exceeded; using page size {0}.'.format(self.PAGE_SIZE))
