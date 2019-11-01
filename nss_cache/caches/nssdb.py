9db2f5a6b6042c6f39f2db69144a39aeb930ae38
Jamie Wilkinson <jaq@spacepants.org>
2019-11-01 11:42:49 +1100

    Oops a typo.

--- nss_cache/caches/nssdb.py
+++ nss_cache/caches/nssdb.py
@@ -1,4 +1,4 @@
-h# Copyright 2007 Google Inc.
+# Copyright 2007 Google Inc.
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
