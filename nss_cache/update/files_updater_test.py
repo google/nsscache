ee301cb1f59356d97e6f330b9c1392f0963896d0
Jamie Wilkinson <jaq@spacepants.org>
2023-05-30 14:56:39 +1000

    refactor: Replace mox with mock in files_updater_test

--- nss_cache/update/files_updater_test.py
+++ nss_cache/update/files_updater_test.py
@@ -25,7 +25,7 @@ import os
 import shutil
 import tempfile
 import unittest
-from mox3 import mox
+from unittest import mock
 
 from nss_cache import config
 from nss_cache import error
@@ -39,7 +39,7 @@ from nss_cache.sources import source
 from nss_cache.update import files_updater
 
 
-class SingleFileUpdaterTest(mox.MoxTestBase):
+class SingleFileUpdaterTest(unittest.TestCase):
     """Unit tests for FileMapUpdater."""
 
     def setUp(self):
@@ -67,14 +67,8 @@ class SingleFileUpdaterTest(mox.MoxTestBase):
             os.system("ls -al %s" % dst_file)
             return dst_file
 
-        dst_file = mox.Value()
-        source_mock = self.mox.CreateMock(source.FileSource)
-        source_mock.GetFile(
-            config.MAP_PASSWORD,
-            mox.Remember(dst_file),
-            current_file=mox.IgnoreArg(),
-            location=mox.IgnoreArg(),
-        ).WithSideEffects(GetFile).AndReturn(dst_file)
+        source_mock = mock.create_autospec(source.FileSource)
+        source_mock.GetFile.side_effect = GetFile
 
         # Construct the cache.
         cache = files.FilesPasswdMapHandler({"dir": self.workdir2})
@@ -89,8 +83,6 @@ class SingleFileUpdaterTest(mox.MoxTestBase):
         )
         updater.WriteModifyTimestamp(original_modify_stamp)
 
-        self.mox.ReplayAll()
-
         self.assertEqual(
             0,
             updater.UpdateCacheFromSource(
@@ -100,8 +92,14 @@ class SingleFileUpdaterTest(mox.MoxTestBase):
 
         self.assertEqual(new_modify_stamp, updater.GetModifyTimestamp())
         self.assertNotEqual(None, updater.GetUpdateTimestamp())
+        source_mock.GetFile.assert_called_with(
+            config.MAP_PASSWORD,
+            mock.ANY,
+            current_file=mock.ANY,
+            location=mock.ANY,
+        )
 
-    @unittest.skip("source map empty during full update")
+    @unittest.skip("raises EmptyMap, is that intended?")
     def testFullUpdateOnEmptyCache(self):
         """A full update as above, but the initial cache is empty."""
         original_modify_stamp = 1
@@ -122,20 +120,21 @@ class SingleFileUpdaterTest(mox.MoxTestBase):
             os.utime(dst_file, (1, 2))
             return dst_file
 
-        source_mock = self.mox.CreateMock(source.FileSource)
-        source_mock.GetFile(
-            config.MAP_PASSWORD, mox.IgnoreArg(), mox.IgnoreArg(), location=None
-        ).WithSideEffects(GetFileEffects)
+        source_mock = mock.create_autospec(source.FileSource)
+        source_mock.GetFile.side_effects = GetFileEffects
 
-        # source_mock = MockSource()
         self.assertEqual(
             0,
             self.updater.UpdateCacheFromSource(
                 cache, source_mock, force_write=False, location=None
             ),
         )
+
         self.assertEqual(new_modify_stamp, self.updater.GetModifyTimestamp())
         self.assertNotEqual(None, self.updater.GetUpdateTimestamp())
+        source_mock.GetFile.assert_called_with(
+            config.MAP_PASSWORD, mock.ANY, mock.ANY, location=None
+        )
 
     def testFullUpdateOnEmptySource(self):
         """A full update as above, but instead, the initial source is empty."""
@@ -155,14 +154,9 @@ class SingleFileUpdaterTest(mox.MoxTestBase):
         password_map.Add(map_entry)
         cache.Write(password_map)
 
-        source_mock = self.mox.CreateMock(source.FileSource)
-        source_mock.GetFile(
-            config.MAP_PASSWORD,
-            mox.IgnoreArg(),
-            current_file=mox.IgnoreArg(),
-            location=None,
-        ).AndReturn(None)
-        self.mox.ReplayAll()
+        source_mock = mock.create_autospec(source.FileSource)
+        source_mock.GetFile.return_value = None
+
         self.assertRaises(
             error.EmptyMap,
             self.updater.UpdateCacheFromSource,
@@ -171,8 +165,15 @@ class SingleFileUpdaterTest(mox.MoxTestBase):
             force_write=False,
             location=None,
         )
+
         self.assertNotEqual(new_modify_stamp, self.updater.GetModifyTimestamp())
         self.assertEqual(None, self.updater.GetUpdateTimestamp())
+        source_mock.GetFile.assert_called_with(
+            config.MAP_PASSWORD,
+            mock.ANY,
+            current_file=mock.ANY,
+            location=None,
+        )
 
     def testFullUpdateOnEmptySourceForceWrite(self):
         """A full update as above, but instead, the initial source is empty."""
@@ -192,26 +193,28 @@ class SingleFileUpdaterTest(mox.MoxTestBase):
         password_map.Add(map_entry)
         cache.Write(password_map)
 
-        source_mock = self.mox.CreateMock(source.FileSource)
-        source_mock.GetFile(
-            config.MAP_PASSWORD,
-            mox.IgnoreArg(),
-            current_file=mox.IgnoreArg(),
-            location=None,
-        ).AndReturn(None)
-        self.mox.ReplayAll()
+        source_mock = mock.create_autospec(source.FileSource)
+        source_mock.GetFile.return_value = None
+
         self.assertEqual(
             0,
             self.updater.UpdateCacheFromSource(
                 cache, source_mock, force_write=True, location=None
             ),
         )
+
         self.assertNotEqual(original_modify_stamp, self.updater.GetModifyTimestamp())
         self.assertNotEqual(None, self.updater.GetUpdateTimestamp())
+        source_mock.GetFile.assert_called_with(
+            config.MAP_PASSWORD,
+            mock.ANY,
+            current_file=mock.ANY,
+            location=None,
+        )
 
 
 @unittest.skip("disabled")
-class AutomountUpdaterTest(mox.MoxTestBase):
+class AutomountUpdaterTest(unittest.TestCase):
     """Unit tests for FileAutomountUpdater class."""
 
     def setUp(self):
@@ -241,7 +244,8 @@ class AutomountUpdaterTest(mox.MoxTestBase):
         )
         self.assertEqual(updater.local_master, False)
 
-    def testUpdate(self):
+    @mock.patch.object(cache_factory, "Create")
+    def testUpdate(self, cache_factory_create_mock):
         """An update gets a master map and updates each entry."""
         map_entry1 = automount.AutomountMapEntry()
         map_entry2 = automount.AutomountMapEntry()
@@ -251,35 +255,28 @@ class AutomountUpdaterTest(mox.MoxTestBase):
         map_entry2.location = "ou=auto.auto,ou=automounts"
         master_map = automount.AutomountMap([map_entry1, map_entry2])
 
-        source_mock = self.mox.CreateMock(zsyncsource.ZSyncSource)
-        source_mock.GetAutomountMasterFile(mox.IgnoreArg()).AndReturn(master_map)
+        source_mock = mock.Mock()
+        source_mock.GetAutomountMasterFile.return_value = master_map
 
         # the auto.home cache
-        cache_mock1 = self.mox.CreateMock(files.FilesCache)
-        cache_mock1.GetCacheFilename().AndReturn(None)
-        cache_mock1.GetMapLocation().AndReturn("/etc/auto.home")
+        cache_mock1 = mock.create_autospec(files.FilesCache)
+        cache_mock1.GetCacheFilename.return_value = None
+        cache_mock1.GetMapLocation.return_value = "/etc/auto.home"
 
         # the auto.auto cache
-        cache_mock2 = self.mox.CreateMock(files.FilesCache)
-        cache_mock2.GetMapLocation().AndReturn("/etc/auto.auto")
-        cache_mock2.GetCacheFilename().AndReturn(None)
+        cache_mock2 = mock.create_autospec(files.FilesCache)
+        cache_mock2.GetMapLocation.return_value = "/etc/auto.auto"
+        cache_mock2.GetCacheFilename.return_value = None
 
         # the auto.master cache
-        cache_mock3 = self.mox.CreateMock(files.FilesCache)
-        cache_mock3.GetMap().AndReturn(master_map)
+        cache_mock3 = mock.create_autospec(files.FilesCache)
+        cache_mock3.GetMap.return_value = master_map
 
-        self.mox.StubOutWithMock(cache_factory, "Create")
-        cache_factory.Create(mox.IgnoreArg(), mox.IgnoreArg(), None).AndReturn(
-            cache_mock3
-        )
-        cache_factory.Create(
-            mox.IgnoreArg(), mox.IgnoreArg(), automount_mountpoint="/auto"
-        ).AndReturn(cache_mock2)
-        cache_factory.Create(
-            mox.IgnoreArg(), mox.IgnoreArg(), automount_mountpoint="/home"
-        ).AndReturn(cache_mock1)
-
-        self.mox.ReplayAll()
+        cache_factory_create_mock.Create.side_effect = [
+            cache_mock3,
+            cache_mock2,
+            cache_mock1,
+        ]
 
         options = {"name": "files", "dir": self.workdir}
         updater = files_updater.FileAutomountUpdater(
@@ -289,8 +286,16 @@ class AutomountUpdaterTest(mox.MoxTestBase):
 
         self.assertEqual(map_entry1.location, "/etc/auto.home")
         self.assertEqual(map_entry2.location, "/etc/auto.auto")
+        cache_factory_create_mock.assert_has_calls(
+            [
+                mock.call(mock.ANY, mock.ANY, None),
+                mock.call(mock.ANY, mock.ANY, automount_mountpoint="/auto"),
+                mock.call(mock.ANY, mock.ANY, automount_mountpoint="/home"),
+            ]
+        )
 
-    def testUpdateNoMaster(self):
+    @mock.patch.object(cache_factory, "Create")
+    def testUpdateNoMaster(self, cache_factory_create_mock):
         """An update skips updating the master map, and approprate sub maps."""
         source_entry1 = automount.AutomountMapEntry()
         source_entry2 = automount.AutomountMapEntry()
@@ -307,48 +312,24 @@ class AutomountUpdaterTest(mox.MoxTestBase):
         local_entry1.location = "/etc/auto.home"
         local_entry2.location = "/etc/auto.null"
         local_master = automount.AutomountMap([local_entry1, local_entry2])
-        source_mock = self.mock()
-        invocation = source_mock.expects(pmock.at_least_once())
-        invocation._CalledUpdateCacheFromSource()
+
+        source_mock = mock.Mock()
         # we should get called inside the DummyUpdater, too.
 
         # the auto.home cache
-        cache_mock1 = self.mock()
+        cache_mock1 = mock.Mock()
         # GetMapLocation() is called, and set to the master map map_entry
-        invocation = cache_mock1.expects(pmock.at_least_once()).GetMapLocation()
-        invocation.will(pmock.return_value("/etc/auto.home"))
-        # we should get called inside the DummyUpdater
-        cache_mock1.expects(pmock.at_least_once())._CalledUpdateCacheFromSource()
+        cache_mock1.GetMapLocation.return_value = "/etc/auto.home"
 
         # the auto.auto cache
-        cache_mock2 = self.mock()
+        cache_mock2 = mock.Mock()
         # GetMapLocation() is called, and set to the master map map_entry
-        invocation = cache_mock2.expects(pmock.at_least_once()).GetMapLocation()
-        invocation.will(pmock.return_value("/etc/auto.auto"))
-        invocation = cache_mock2.expects(
-            pmock.at_least_once()
-        )._CalledUpdateCacheFromSource()
+        cache_mock2.GetMapLocation.return_value = "/etc/auto.auto"
         # the auto.master cache, which should not be written to
-        cache_mock3 = self.mock()
-        invocation = cache_mock3.expects(pmock.once())
-        invocation = invocation.method("GetMap")
-        invocation.will(pmock.return_value(local_master))
-        invocation = cache_mock3.expects(pmock.once())
-        invocation = invocation.method("GetMap")
-        invocation.will(pmock.return_value(local_master))
+        cache_mock3 = mock.Mock()
+        cache_mock3.GetMap.return_value = local_master
 
-        cache_mocks = {"/home": cache_mock1, "/auto": cache_mock2, None: cache_mock3}
-
-        # Create needs to return our mock_caches
-        def DummyCreate(
-            unused_cache_options, unused_map_name, automount_mountpoint=None
-        ):
-            # the order of the master_map iterable is not predictable, so we use the
-            # automount_mountpoint as the key to return the right one.
-            return cache_mocks[automount_mountpoint]
-
-        original_create = cache_factory.Create
-        cache_factory.Create = DummyCreate
+        cache_factory_create_mock.side_effect = [cache_mock1, cache_mock2, cache_mock3]
 
         skip = files_updater.FileAutomountUpdater.OPT_LOCAL_MASTER
         options = {skip: "yes", "dir": self.workdir}
@@ -357,29 +338,23 @@ class AutomountUpdaterTest(mox.MoxTestBase):
         )
         updater.UpdateFromSource(source_mock)
 
-        cache_factory.Create = original_create
+        source_mock._CalledUpdateCacheFromSource.assert_called_once()
+        # we should get called inside the DummyUpdater
+        cache_mock1._CalledUpdateCacheFromSource.assert_called_once()
+        cache_mock2._CalledUpdateCacheFromSource().assert_called_once()
 
-    def testUpdateCatchesMissingMaster(self):
+    @mock.patch.object(cache_factory, "Create")
+    def testUpdateCatchesMissingMaster(self, cache_factory_create_mock):
         """Gracefully handle a missing local master map."""
         # use an empty master map from the source, to avoid mocking out already
         # tested code
-        source_mock = self.mock()
+        source_mock = mock.Mock
 
-        cache_mock = self.mock()
+        cache_mock = mock.Mock()
         # raise error on GetMap()
-        invocation = cache_mock.expects(pmock.once()).GetMap()
-        invocation.will(pmock.raise_exception(error.CacheNotFound))
+        cache_mock.GetMap.side_effect = error.CacheNotFound
 
-        # Create needs to return our mock_cache
-        def DummyCreate(
-            unused_cache_options, unused_map_name, automount_mountpoint=None
-        ):
-            # the order of the master_map iterable is not predictable, so we use the
-            # automount_mountpoint as the key to return the right one.
-            return cache_mock
-
-        original_create = cache_factory.Create
-        cache_factory.Create = DummyCreate
+        cache_factory_create_mock.return_value = cache_mock
 
         skip = files_updater.FileAutomountUpdater.OPT_LOCAL_MASTER
         options = {skip: "yes", "dir": self.workdir}
@@ -387,11 +362,7 @@ class AutomountUpdaterTest(mox.MoxTestBase):
             config.MAP_AUTOMOUNT, self.workdir, options
         )
 
-        return_value = updater.UpdateFromSource(source_mock)
-
-        self.assertEqual(return_value, 1)
-
-        cache_factory.Create = original_create
+        self.assertEqual(1, updater.UpdateFromSource(source_mock))
 
 
 if __name__ == "__main__":
